4032c80841909fa4b6caa31057fb0ae97ab99f4b
pranav82k <pranav82k@gmail.com>
2022-11-29 19:25:06 +0530

    Conditional API BASEURL according to host

--- src/utlis/baseUrl.js
+++ src/utlis/baseUrl.js
@@ -1,3 +1,5 @@
-const baseUrl = "http://localhost:5000";
+const hostname = window.location.hostname;
 
+const baseUrl = hostname === "localhost" ? "http://localhost:5000" : "https://pk-mern-blog-apis.herokuapp.com";
+// console.log(baseUrl);
 export default baseUrl;
