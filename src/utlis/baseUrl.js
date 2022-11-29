5ca9b523465b76742f7aa065916e376c16e2320f
pranav82k <pranav82k@gmail.com>
2022-11-29 19:31:06 +0530

    Removed console from baseURL and added redirect file in public folder to fix 404

--- src/utlis/baseUrl.js
+++ src/utlis/baseUrl.js
@@ -1,5 +1,5 @@
 const hostname = window.location.hostname;
 
 const baseUrl = hostname === "localhost" ? "http://localhost:5000" : "https://pk-mern-blog-apis.herokuapp.com";
-console.log(baseUrl);
+// console.log(baseUrl);
 export default baseUrl;
